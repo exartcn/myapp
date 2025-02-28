88efd5ba50730deb714b0d4acb68793a9d7f5a58
unknown <tikK76111@tisnt.tis.co.jp>
2025-02-28 22:28:30 +0900

    deeplink task

--- app_https.js
+++ app_https.js
@@ -13,6 +13,26 @@ const options = {
 
 app.use('/.well-known', express.static(path.join(__dirname, 'public')));
 
+app.get('/deeplink:id', (req, res) => {
+    const userAgent = req.headers['user-agent'].toLowerCase();
+    console.log(userAgent);
+    
+    if (/iphone|ipad|ipod/.test(userAgent)) {
+        // iOS デバイス - App Store へリダイレクト
+        res.redirect('https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864');
+    } else if (/android/.test(userAgent)) {
+        // Android デバイス - Google Play へリダイレクト
+        res.redirect('https://play.google.com/store/apps/details?id=com.ss.android.ugc.trill');
+    } else if (/windows/.test(userAgent)) {
+      res.redirect('http://26.101.136.178/cmm/auth/GWA90/GWA9003');
+    } else if (/Mac/.test(userAgent)) {
+      res.redirect('https://play.google.com/store/apps/details?id=com.ss.android.ugc.trill');
+    } else {
+        // その他のデバイス - デフォルトページを表示
+        res.send('iOS または Android デバイスでアクセスしてください。');
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
