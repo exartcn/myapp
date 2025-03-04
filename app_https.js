942cbebe11ba27243180f0ba6d0723c2e8cf6225
unknown <tikK76111@tisnt.tis.co.jp>
2025-03-04 15:52:34 +0900

    deeplink task

--- app_https.js
+++ app_https.js
@@ -18,18 +18,18 @@ app.get('/deeplink/:id', (req, res) => {
     console.log(userAgent);
     
     if (/iphone|ipad|ipod/.test(userAgent)) {
-        // iOS デバイス - App Store へリダイレクト
-        res.redirect('https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864');
+      // iOS デバイス - App Store へリダイレクト
+      res.redirect('https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864');
     } else if (/android/.test(userAgent)) {
-        // Android デバイス - Google Play へリダイレクト
-        res.redirect('https://play.google.com/store/apps/details?id=com.ss.android.ugc.trill');
-    } else if (/windows/.test(userAgent)) {
-      res.redirect('http://26.101.136.178/cmm/auth/GWA90/GWA9003');
-    } else if (/Mac/.test(userAgent)) {
+      // Android デバイス - Google Play へリダイレクト
       res.redirect('https://play.google.com/store/apps/details?id=com.ss.android.ugc.trill');
+    } else if (/windows/.test(userAgent)) {
+      res.redirect('https://apps.microsoft.com/detail/9NH2GPH4JZS4?hl=neutral&gl=JP&ocid=pdpshare');
+    } else if (/mac/.test(userAgent)) {
+      res.redirect('https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864');
     } else {
-        // その他のデバイス - デフォルトページを表示
-        res.send('iOS または Android デバイスでアクセスしてください。');
+      // その他のデバイス - デフォルトページを表示
+      res.send('iOS または Android デバイスでアクセスしてください。');
     }
 });
 
