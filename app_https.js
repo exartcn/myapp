85bd11a9168be740103041ed1c8f4ce8a318ade7
unknown <tikK76111@tisnt.tis.co.jp>
2025-03-04 16:05:22 +0900

    deeplink task

--- app_https.js
+++ app_https.js
@@ -19,14 +19,14 @@ app.get('/deeplink/:id', (req, res) => {
     
     if (/iphone|ipad|ipod/.test(userAgent)) {
       // iOS デバイス - App Store へリダイレクト
-      res.redirect('https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864');
+      res.redirect('itms-apps://apps.apple.com/app/id1235601864');
     } else if (/android/.test(userAgent)) {
       // Android デバイス - Google Play へリダイレクト
-      res.redirect('https://play.google.com/store/apps/details?id=com.ss.android.ugc.trill');
+      res.redirect('market://details?id=com.zhiliaoapp.musically');
     } else if (/windows/.test(userAgent)) {
-      res.redirect('https://apps.microsoft.com/detail/9NH2GPH4JZS4?hl=neutral&gl=JP&ocid=pdpshare');
+      res.redirect('ms-windows-store://pdp/?ProductId=9NH2GPH4JZS4');
     } else if (/mac/.test(userAgent)) {
-      res.redirect('https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864');
+      res.redirect('macappstore://itunes.apple.com/app/id1235601864');
     } else {
       // その他のデバイス - デフォルトページを表示
       res.send('iOS または Android デバイスでアクセスしてください。');
