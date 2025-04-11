a76fdae702d54e17d0d77875187641252a992088
unknown <tikK76111@tisnt.tis.co.jp>
2025-04-11 10:57:10 +0900

    deeplink task

--- app_https.js
+++ app_https.js
@@ -21,7 +21,7 @@ const combinedConfig = {
       "details": [
         {
           "appID": "7Y9M87SSC3.com.tis.test76",
-          "paths": ["/deeplinkb/*"]
+          "paths": ["/cmm-bff/common/auth/deeplink/*"]
         }
       ]
     }
@@ -31,7 +31,7 @@ const combinedConfig = {
       "relation": ["delegate_permission/common.handle_all_urls"],
       "target": {
         "namespace": "android_app",
-        "package_name": "com.test76",
+        "package_name": "com.JHFAuthenticator",
         "sha256_cert_fingerprints": [
           "FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C"
         ]
@@ -75,7 +75,7 @@ app.get('/deeplink/:id', (req, res) => {
     }
 });
 
-app.get('/deeplinkb/:id', (req, res) => {
+app.get('/cmm-bff/common/auth/deeplink/:id', (req, res) => {
   const userAgent = req.headers['user-agent'].toLowerCase();
   console.log(userAgent);
   
