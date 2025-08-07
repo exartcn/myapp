52bf22d3d79ea34868d862068f416821fee69549
unknown <tikK76111@tisnt.tis.co.jp>
2025-08-07 15:53:23 +0900

    deeplink task

--- app_https.js
+++ app_https.js
@@ -116,31 +116,31 @@ const combinedConfig = {
 //   }
 // });
 
-app.get("/cmm-bff/common/auth/deeplink/:id?", (req, res) => {
-  const userAgent = req.headers["user-agent"].toLowerCase();
-  console.log(userAgent);
+// app.get("/cmm-bff/common/auth/deeplink/:id?", (req, res) => {
+//   const userAgent = req.headers["user-agent"].toLowerCase();
+//   console.log(userAgent);
 
-  if (/iphone|ipad|ipod/.test(userAgent)) {
-    // iOS デバイス - App Store へリダイレクト
-    res.redirect(
-      "https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864"
-    );
-  } else if (/android/.test(userAgent)) {
-    // Android デバイス - Google Play へリダイレクト
-    res.redirect(
-      "https://play.google.com/store/apps/details?id=com.ss.android.ugc.trill"
-    );
-  } else if (/windows/.test(userAgent)) {
-    res.redirect("ms-windows-store://pdp/?ProductId=9nh2gph4jzs4");
-  } else if (/mac/.test(userAgent)) {
-    res.redirect(
-      "macappstore://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864"
-    );
-  } else {
-    // その他のデバイス - デフォルトページを表示
-    res.send("iOS または Android デバイスでアクセスしてください。");
-  }
-});
+//   if (/iphone|ipad|ipod/.test(userAgent)) {
+//     // iOS デバイス - App Store へリダイレクト
+//     res.redirect(
+//       "https://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864"
+//     );
+//   } else if (/android/.test(userAgent)) {
+//     // Android デバイス - Google Play へリダイレクト
+//     res.redirect(
+//       "https://play.google.com/store/apps/details?id=com.ss.android.ugc.trill"
+//     );
+//   } else if (/windows/.test(userAgent)) {
+//     res.redirect("ms-windows-store://pdp/?ProductId=9nh2gph4jzs4");
+//   } else if (/mac/.test(userAgent)) {
+//     res.redirect(
+//       "macappstore://apps.apple.com/jp/app/tiktok-%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%88%E3%83%83%E3%82%AF/id1235601864"
+//     );
+//   } else {
+//     // その他のデバイス - デフォルトページを表示
+//     res.send("iOS または Android デバイスでアクセスしてください。");
+//   }
+// });
 
 
 
