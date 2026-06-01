ac60f612f8d736f08292f2c9e86268dfededb1a5
unknown <tikK76111@tisnt.tis.co.jp>
2026-06-01 13:30:20 +0900

    Add JSON upload routes

--- app_https.js
+++ app_https.js
@@ -5,6 +5,17 @@ const path = require("path");
 
 const app = express();
 app.use(express.json({ limit: "10mb" }));
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 let uploadedJson = null;
 
